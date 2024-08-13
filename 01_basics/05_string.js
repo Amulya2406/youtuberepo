16299a5da487c2fcc22a5e43653fa9da7ac37058
Amulya2406 <137594859+Amulya2406@users.noreply.github.com>
2024-08-13 17:56:55 +0000

    sttring

---
+++ 01_basics/05_string.js
@@ -0,0 +1,34 @@
+const name="amulyanayak"
+const num =50
+
+//console.log(name+ num + "value")
+
+console.log(`hello im ${name}, im ${num} years old `)
+
+console.log(name.length)
+console.log(name[2])
+console.log(name.__proto__)
+console.log(name.toUpperCase())
+console.log(name.indexOf('m'))
+
+
+const newString= name.substring(0,4)
+console.log(newString)
+
+const newStrings= name.slice(-8,5)
+console.log(newStrings)
+
+const newStrings1= " amulya  "
+console.log(newStrings1)
+console.log(newStrings1.trim())
+
+
+const url = "https://amulya%20nayak"
+console.log(url.replace('%20','-'))
+console.log(url.includes('%20'))
+console.log(url.split('a'))
+
+//learn more about String by myself
+
+
+
