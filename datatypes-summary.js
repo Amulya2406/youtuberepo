f1cc4918f3df0126dae5ca019eba4469d298ddb6
Amulya2406 <137594859+Amulya2406@users.noreply.github.com>
2024-08-13 05:47:37 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,24 @@
+//primitive :call by value
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//reference 
+//Array, objects, functions
+
+const id= Symbol('123')
+const id2= Symbol('123')
+
+console.log(id === id2)
+
+
+const fruit =["mango","orange","apple"]
+let myobj ={
+    name: "amu",
+    age : 22,
+}
+
+const myFn= function(){
+    console.log("hello world");
+}
+
+console.log(typeof bigNumber)
+
