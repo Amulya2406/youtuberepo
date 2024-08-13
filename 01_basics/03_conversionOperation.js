75e3b947c975e7ef16fc13989667004ce65593a1
Amulya2406 <137594859+Amulya2406@users.noreply.github.com>
2024-08-13 03:02:39 +0000

    conversionOFDatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score ="33"
+
+console.log(score)
+console.log(typeof(score))
+
+//confirm.log("---------------")
+let scores = "33a"
+let valInNum = Number(scores)
+console.log(scores);
+console.log(valInNum)
+console.log(typeof valInNum)
+console.log(typeof(scores))
+
+
+//"33" = 33
+//"3a"= NaN i.e not an number
+
+let isLoggedIn =1
+let booleanISLogged = Boolean(isLoggedIn)
+console.log(booleanISLogged)
+
+let isLoggedIn2 =""
+let booleanISLogged2 = Boolean(isLoggedIn2)
+console.log(booleanISLogged2)
+
+let isLoggedIn3 ="amu"
+let booleanISLogged3 = Boolean(isLoggedIn3)
+console.log(booleanISLogged3)
+
+let score3=33
+let valInNum3 = String(score3)
+console.log(valInNum3);
+console.log(typeof valInNum3);
+
