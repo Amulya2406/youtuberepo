10a678c2aeba18adc525988e95a5a3ed0e718489
Amulya2406 <137594859+Amulya2406@users.noreply.github.com>
2024-08-13 02:40:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =144553;
+let accountEmail= "amulya@google.com";
+var accountPAsswd= "12345"
+accCity="mangalore" //possible but dont use 
+let accState;
+
+//accountId =2
+accountEmail="hc@hc.com"
+accountPAsswd="2222"
+accCity="bnglr"
+
+console.log(accountId);
+/* prefer not to use VAR. bcs of issue in block scope and fn scope */
+
+console.table([accountId,accountEmail,accountPAsswd,accCity,accState])
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js as newer cersion
+
+//alert(3+2) //cant use. gives error. using nodejs not browser
+
+console.log(3+3)
+console.log("amu") //cant write consoles in same line
+
+let name ="amu"
+let age = 18
+let isLoggedIn = false
+
+//number 2 to the power 53
+//bigint
+//string= ""
+//boolean= T/F
+//null= standalone value
+//undefined= 
+//symbol= unique
+
+//object
+
+console.log(typeof "Amu")
+
+
