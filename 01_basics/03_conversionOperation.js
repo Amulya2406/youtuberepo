2eda1cf5420e7a1ef5731bebbdcb66274cb9ac1c
Amulya2406 <137594859+Amulya2406@users.noreply.github.com>
2024-08-13 05:04:30 +0000

    updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,3 +32,33 @@ let valInNum3 = String(score3)
 console.log(valInNum3);
 console.log(typeof valInNum3);
 
+//*************************************************** 
+
+let value =3
+let negvalue = -value
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1="Amu"
+let str2="lya"
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"1")
+
+
+
+
+
+
+
