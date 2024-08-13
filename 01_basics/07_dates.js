2f94127ceb27f843bbb4e307aa43719331216865
Amulya2406 <137594859+Amulya2406@users.noreply.github.com>
2024-08-13 17:57:11 +0000

    date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,25 @@
+let mydate= new Date()
+console.log(mydate)
+console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toLocaleDateString())
+console.log(typeof mydate)
+
+
+//let myd = new Date(2023,0,23)
+//let myd = new Date(2023,0,23,5,3)
+let myd = new Date("2023-01-23")
+
+console.log(myd.toLocaleString())
+
+let myt = Date.now()
+console.log(myt)
+console.log(myd.getTime())
+
+let newdate = new Date()
+console.log(newdate);
+console.log(newdate.getDate());
+console.log(newdate.getMonth()+1);
+
+//much more
+
